7a06fc1b35758ac9892047f60726a8eb431540b3
Adam Coddington <me@adamcoddington.net>
2013-08-29 21:30:21 -0700

    Bumping version number.

--- teamspeaknotifier/__init__.py
+++ teamspeaknotifier/__init__.py
@@ -21,7 +21,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 __author__ = 'Adam Coddington <me@adamcoddington.net>'
-__version__ = (1, 1, 0)
+__version__ = (1, 1, 1)
 
 def run_from_cmdline():
     import logging
